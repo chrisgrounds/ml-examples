cd263739eb94031d5823896f769a18e097276d31
Chris Grounds <chrisbacon2009@hotmail.com>
2021-04-12 22:22:38 +0100

    made predicton box padding more consistent

--- components/PredictionBox.js
+++ components/PredictionBox.js
@@ -1,7 +1,7 @@
 const PredictionBox = ({ prediction }) => (
   <>
     {prediction && (
-      <h2 className="pt-8 text-blue-600 text-4xl">{prediction}</h2>
+      <h2 className="p-4 text-blue-600 text-4xl">{prediction}</h2>
     )}
   </>
 );
